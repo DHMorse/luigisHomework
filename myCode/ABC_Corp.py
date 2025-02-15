215e4bbb7dd5e2de2c423b07ab9d2e1f115e8b07
DHMorse <daniel@morsetech.net>
2025-02-14 20:23:57 -0600

    I updated the ABC_Corp file and I'm proud of it.

--- myCode/ABC_Corp.py
+++ myCode/ABC_Corp.py
@@ -1,7 +1,6 @@
 # ABC_Corp.py
 
-# This code needs to be cleaned up, it works, but I don't like the structure of it
-# I don't think a class is needed for this
+from typing import List, Dict
 
 class Customer:
     firstName: str = ''
@@ -12,46 +11,94 @@ class Customer:
     zipCode: str = ''
     phoneNumber: str = ''
 
-def main(ABC_names: str = "myCode/ABC_names.txt", ABC_addresses: str = "myCode/ABC_addresses.txt", 
-            ABC_phone: str = "myCode/ABC_phone.txt", 
-            ABC_customerData: str = "myCode/ABC_Customer_Data.txt"
-        ) -> None:
-    
-    customers: list[str] = []
-    
-    customerObj = Customer()
+def extractNames(filename: str) -> List[str]:
+    with open(filename, 'r') as namesFile:
+        names = namesFile.readlines()
+        return [' '.join(name.strip().split()[::-1]) for name in names]
 
-    with open(ABC_names, "r") as ABC_names:
-        name = ABC_names.readline().strip()
+def extractAddresses(filename: str) -> List[Dict[str, str]]:
+    addresses = []
     
-    with open(ABC_addresses, "r") as ABC_addresses:
-        customerObj.address = ABC_addresses.readline().strip()
-        customerObj.city = ABC_addresses.readline().strip()
-        line3 = ABC_addresses.readline().strip()
+    with open(filename, 'r') as file:
+        lines = file.readlines()
     
-    with open(ABC_phone, "r") as ABC_phone:
-        customerObj.phoneNumber = ABC_phone.readline().strip()
+    # Ensure the number of lines is a multiple of 3
+    if len(lines) % 3 != 0:
+        raise ValueError("File format is incorrect. Each address should be three lines.")
+    
+    for i in range(0, len(lines), 3):
+        address = lines[i].strip()
+        city = lines[i+1].strip()
+        state_zip = lines[i+2].strip().split()
+        
+        if len(state_zip) != 2:
+            raise ValueError(f"Invalid state/ZIP format on line {i+3}: {lines[i+2]}")
+        
+        state, zip_code = state_zip
+        
+        addresses.append({
+            'address': address,
+            'city': city,
+            'state': state,
+            'zip_code': zip_code
+        })
+    
+    return addresses
 
-    for i in range(1000):
-        names = name.split()
-        
-        customerObj.firstName = names[0]
-        customerObj.lastName = names[1]
-        
-        stzip = line3.split()
-        
-        customerObj.state = stzip[0]
-        customerObj.zipCode = stzip[1]
-        
+def extractPhoneNumbers(filename: str) -> List[str]:
+    with open(filename, 'r') as phoneFile:
+        phoneNumbers = phoneFile.readlines()
+        return [phone.strip() for phone in phoneNumbers]
+
+def writeCustomerData(filepath: str, customers: List[Customer]) -> None:
+    with open(filepath, 'w') as customerDataFile:
+        for customer in customers:
+            customerDataFile.write(f"{customer.lastName}, {customer.firstName}\n")
+            customerDataFile.write(f"{customer.address}\n")
+            customerDataFile.write(f"{customer.city}, {customer.state} {customer.zipCode}\n")
+            customerDataFile.write(f"{customer.phoneNumber}\n\n")
+
+def makeCustomerObjectList(firstAndLastNames: List[str], addresses: List[str], cities: List[str], states: List[str], zipCodes: List[str], 
+                            phoneNumbers: List[str]) -> List[Customer]:
+    customers: List[str] = []
+    
+    if not len(firstAndLastNames) == len(addresses) == len(cities) == len(states) == len(zipCodes) == len(phoneNumbers):
+        raise ValueError("Data files do not contain the same number of records.")
+    
+    if not type(firstAndLastNames) == type(addresses) == type(cities) == type(states) == type(zipCodes) == type(phoneNumbers) == list:
+        raise ValueError("Data files must be lists of strings.")
+
+    for i in range(len(firstAndLastNames)):
+        customerObj = Customer()
+        customerObj.firstName = firstAndLastNames[i].split()[0]
+        customerObj.lastName = firstAndLastNames[i].split()[1]
+        customerObj.address = addresses[i]
+        customerObj.city = cities[i]
+        customerObj.state = states[i]
+        customerObj.zipCode = zipCodes[i]
+        customerObj.phoneNumber = phoneNumbers[i]
         customers.append(customerObj)
 
-    with open(ABC_customerData, "w") as ABC_customerData:
-        for customerObj in customers:
-            ABC_customerData.write(f"{customerObj.lastName}, {customerObj.firstName}\n")
-            ABC_customerData.write(f"{customerObj.address}\n")
-            ABC_customerData.write(f"{customerObj.city}, {customerObj.state} {customerObj.zipCode}\n")
-            ABC_customerData.write(f"{customerObj.phoneNumber}\n\n")
+    return customers
 
+def main(ABC_namesPath: str = "myCode/ABC_names.txt", ABC_addressesPath: str = "myCode/ABC_addresses.txt", 
+            ABC_phonePath: str = "myCode/ABC_phone.txt", 
+            ABC_customerDataPath: str = "myCode/ABC_Customer_Data.txt"
+        ) -> None:
+    firstAndLastNames: List[str] = extractNames(ABC_namesPath)
+
+    addressData: List[Dict[str, str]] = extractAddresses(ABC_addressesPath)
+    
+    addresses = [address['address'] for address in addressData]
+    cities = [address['city'] for address in addressData]
+    states = [address['state'] for address in addressData]
+    zipCodes = [address['zip_code'] for address in addressData]
+
+    phoneNumbers: List[str] = extractPhoneNumbers(ABC_phonePath)
+
+    customers: List[Customer] = makeCustomerObjectList(firstAndLastNames, addresses, cities, states, zipCodes, phoneNumbers)
+
+    writeCustomerData(ABC_customerDataPath, customers)
 
 if __name__ == "__main__":
     main()
